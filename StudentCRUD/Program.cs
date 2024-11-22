8176e538d41aec3e40f14d804f3d084287e398be
Nigora2509 <yarmukhamedovanigora@gmail.com>
2024-11-23 01:29:32 +0500

    studentCRUD

---
+++ StudentCRUD/Program.cs
@@ -0,0 +1,97 @@
+﻿using StudentCRUD.Services;
+using StudentCRUD.Models;
+
+namespace StudentCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFrontEnd();
+        }
+        public static void RunFrontEnd()
+        {
+            var studentService = new StudentService();
+            while (true)
+            {
+                Console.WriteLine(" 1. Add");
+                Console.WriteLine(" 2. Delete");
+                Console.WriteLine(" 3. Update");
+                Console.WriteLine(" 4. GetAll");
+                Console.WriteLine(" 5. GetById");
+                Console.Write(" Enter option: ");
+                var option  = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    var student = new Student();
+                    Console.Write(" Enter name :");
+                    student.FirstName = Console.ReadLine();
+                    Console.Write(" Enter Last name: ");
+                    student.LastName = Console.ReadLine();
+                    Console.Write("Enter Age : ");
+                    student.Age = int.Parse(Console.ReadLine());
+                    Console.Write(" Enter Adress: ");
+                    student.Adress = Console.ReadLine();
+                    Console.Write(" Enter Faculti: ");
+                    student.Faculty = Console.ReadLine();
+
+                    studentService.AddStudent(student);
+                }
+                else if(option == "2")
+                {
+                    Console.Write(" Enter Id to Delete: ");
+                    var id = Guid.Parse(Console.ReadLine());
+                    var studentdelete = studentService.DeleteStudent(id);
+                    Console.WriteLine(studentdelete);
+                }
+                else if (option == "3")
+                {
+
+                    var student = new Student();
+                    Console.Write(" Enter Id to update: ");
+                    student.Id = Guid.Parse(Console.ReadLine());
+                    Console.Write(" Enter name :");
+                    student.FirstName = Console.ReadLine();
+                    Console.Write(" Enter Last name: ");
+                    student.LastName = Console.ReadLine();
+                    Console.Write("Enter Age : ");
+                    student.Age = int.Parse(Console.ReadLine());
+                    Console.Write(" Enter Adress: ");
+                    student.Adress = Console.ReadLine();
+                    Console.Write(" Enter Faculti: ");
+                    student.Faculty = Console.ReadLine();
+
+                    studentService.UpdateStudent(student);
+                }
+                else if (option == "4")
+                {
+                    var students = studentService.GetAllStudents();
+                    foreach (var student in students)
+                    {
+                        string studentInfo = $" Id : {student.Id}, FirstName: {student.FirstName}, " +
+                            $" LastName: {student.LastName} , Age: {student.Age}, Adress: {student.Adress}," +
+                            $" Faculty: {student.Faculty}";
+                        Console.WriteLine($" {studentInfo} ");
+                    }
+                }
+                else if (option == "5")
+                {
+                    Console.Write(" Enter Id to get : ");
+                    var id = Guid.Parse (Console.ReadLine());
+                    var student = studentService.GetById(id);
+                    string studentInfo = $" Id : {student.Id}, FirstName: {student.FirstName}, " +
+                           $" LastName: {student.LastName} , Age: {student.Age}, Adress: {student.Adress}," +
+                           $" Faculty: {student.Faculty}";
+                    Console.WriteLine($" {studentInfo} ");
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+                        
+            }
+        }
+
+    }
+}
---
+++ StudentCRUD/Services/StudentService.cs
@@ -0,0 +1,103 @@
+﻿using StudentCRUD.Models;
+
+namespace StudentCRUD.Services
+{
+    public class StudentService
+    {
+        private List<Student> students;
+        public StudentService()
+        {
+            students = new List<Student>();
+            DataSeed();
+        }
+
+        public Student AddStudent(Student student)
+        {
+            student.Id = Guid.NewGuid();
+            students.Add(student);
+            return student;
+        }
+
+        public bool DeleteStudent(Guid studentId)
+        {
+            var exists = false;
+            foreach (var student in students)
+            {
+                if (student.Id == studentId)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
+
+        public bool UpdateStudent(Student updateStudent)
+        {
+            for (var i = 0; i < students.Count; i++)
+            {
+                if (students[i].Id == updateStudent.Id)
+                {
+                    students[i] = updateStudent;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Student GetById(Guid studentId)
+        {
+            foreach (var student in students)
+            {
+                if (student.Id == studentId)
+                {
+                    return student;
+
+                }
+            }
+            return null;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return students;
+
+        }
+
+        private void DataSeed()
+        {
+            var firstStudent = new Student();
+            {
+                firstStudent.Id = Guid.NewGuid();
+                firstStudent.FirstName = "Alisher";
+                firstStudent.LastName = "Umarov";
+                firstStudent.Age = 21;
+                firstStudent.Adress = "Olmazor street 34";
+                firstStudent.Faculty = "IT";
+
+            }
+            var secondStudent = new Student();
+            {
+                secondStudent.Id = Guid.NewGuid();
+                secondStudent.FirstName = "Umida";
+                secondStudent.LastName = "Odilova";
+                secondStudent.Age = 20;
+                secondStudent.Adress = "Beruniy 12";
+                secondStudent.Faculty = " Languages";
+
+            }
+            var thirdStudent = new Student();
+            {
+                thirdStudent.Id = Guid.NewGuid();
+                thirdStudent.FirstName = "Sevara";
+                thirdStudent.LastName = "Saidova";
+                thirdStudent.Age = 20;
+                thirdStudent.Adress = "beruniy 24";
+                thirdStudent.Faculty = "IT";
+            }
+            students.Add(firstStudent);
+            students.Add(secondStudent);
+            students.Add(thirdStudent);
+        }
+    }
+}
